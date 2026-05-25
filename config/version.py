59a7b525d1b2541cc09ccd5d2473a8d757b30560
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-05-25 11:33:24 +0200

    feat: providing all information about the program in to version.py

---
+++ config/version.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+PROGRAM_NAME = "Program do drukowania raportów cięcia folii"
+PROGRAM_VERSION = "1.0.0"
+PROGRAM_YEAR = datetime.now().year
+PROGRAM_AUTHOR = "Jarosław Sochacki"
+DESCRIPTION = ("Program pobiera dane z pliku JSON, następnie generuje raport cięcia folii\n")
+COMPANY_MAIL = "JSochacki@salamander-windows.com"
+PRIVATE_MAIL = "inz.jaroslawsochacki@gmail.com"
