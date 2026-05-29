cc5689f9f5661606ba8c9bfe46486f7bbc7b7bfc
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-05-29 12:29:47 +0200

    fix: replace the path text on the server with a variable

--- deploy/deploy_logic.py
+++ deploy/deploy_logic.py
@@ -4,6 +4,7 @@ import hashlib
 import threading
 import sys
 import json
+from paths import BUILD_PATH, LATEST_JSON_PATH
 from pathlib import Path
 
 class ReleaseBuilder:
@@ -118,7 +119,7 @@ class ReleaseBuilder:
     def _upload_to_server(self, local_zip_path: Path) -> str:
         self.log("Krok 4: Kopiowanie archiwum na dysk sieciowy...")
         
-        target_dir = Path(r"\\na02\groups\Produkcja\Planowanie OKL\Production Counter Program\FoilReports\builds")
+        target_dir = Path(BUILD_PATH)
         target_path = target_dir / local_zip_path.name
         
         shutil.copy2(local_zip_path, target_path)
@@ -127,7 +128,7 @@ class ReleaseBuilder:
 
     def _update_latest_json(self, sha256_hash: str, target_zip_path: str):
         self.log("Krok 5: Aktualizacja pliku latest.json...")
-        latest_json_path = Path(r"\\na02\groups\Produkcja\Planowanie OKL\Production Counter Program\FoilReports\latest.json")
+        latest_json_path = Path(LATEST_JSON_PATH)
         
         try:
         
