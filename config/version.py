713fb8587034e94bef9f48fd52f3528ec3edcd88
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-06-18 14:20:24 +0200

    Program update to the version 1.2.0

--- config/version.py
+++ config/version.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 PROGRAM_NAME = "Program do drukowania raportów cięcia folii"
-PROGRAM_VERSION = "1.1.9"
+PROGRAM_VERSION = "1.2.0"
 PROGRAM_YEAR = datetime.now().year
 PROGRAM_AUTHOR = "Jarosław Sochacki"
 DESCRIPTION = ("Program pobiera dane z pliku JSON, następnie generuje raport cięcia folii\n")
