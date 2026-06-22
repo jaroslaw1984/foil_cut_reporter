9124c7165764abab813da511618b6b91277ca7ef
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-06-22 12:44:01 +0200

    chore: comment correction

--- config/paths.py
+++ config/paths.py
@@ -19,5 +19,5 @@ LATEST_JSON_PATH = r"\\na02\groups\3.PROJEKTY\Production Counter Program\FoilRep
 # --- ścieżka do folderu z historią raportów ---
 HISTORY_PATH = r"\\na02\groups\3.PROJEKTY\Production Counter Program\FoilReports\history"
 
-# --- ścieżka z informacjami o niestandardowych foliowych papierach ---
+# --- ścieżka z informacjami o niestandardowych foliach papierowych ---
 PAPER_FOILS = BASE_DIR / "config" / "paper_foils.json"
